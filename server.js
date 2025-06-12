4d4526d7ef8a1e2d7d7cc893ab18a819008fdb2b
unknown <ktaher642@gmail.com>
2025-06-12 11:54:58 +0530

    remove ip address

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.use("/api/admin/review", adminReviewRouter);
 app.use("/api/admin/token", adminTokenRouter);
 
 mongoose.connect(process.env.MONGODB_URI).then(() => {
-  app.listen(PORT, "192.168.1.43", async () => {
+  app.listen(PORT, async () => {
     console.log(`Listening on port ${PORT}`);
     console.log("Database connection established");
     schedular();
