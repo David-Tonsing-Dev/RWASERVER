83eb57dcab13e14fccddd6a03231293de756c965
unknown <ktaher642@gmail.com>
2025-06-28 16:42:00 +0530

    fix bug

--- controllers/tokenController.js
+++ controllers/tokenController.js
@@ -190,10 +190,10 @@ const getAllToken = async (req, res) => {
     }
 
     const sortOptions = { [sortBy]: order };
-    const query = {
-      [sortBy]: { $ne: null },
-      enable: true,
-    };
+    // const query = {
+    //   [sortBy]: { $ne: null },
+    //   enable: true,
+    // };
 
     if (filter) {
       const getToken = await Token.find({
@@ -232,14 +232,14 @@ const getAllToken = async (req, res) => {
 
     // const skip = (page - 1) * size;
 
-    const getTokens = await Token.find(query)
+    const getTokens = await Token.find()
       .sort(sortOptions)
       .skip((page - 1) * size)
       .limit(size)
       .populate("category", "categoryName")
       .lean();
 
-    const tokenCount = await Token.countDocuments(query);
+    const tokenCount = await Token.countDocuments();
 
     return res
       .status(200)
